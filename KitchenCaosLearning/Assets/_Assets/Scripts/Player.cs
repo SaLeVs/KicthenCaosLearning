05530f854bf5826b1c8fbe72012a80e164a15785
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-22 14:31:13 -0300

    v8
    Input system refactor

--- KitchenCaosLearning/Assets/_Assets/Scripts/Player.cs
+++ KitchenCaosLearning/Assets/_Assets/Scripts/Player.cs
@@ -2,37 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
+
     private float moveSpeed = 5f;
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0,0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        inputVector = inputVector.normalized;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 30f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
