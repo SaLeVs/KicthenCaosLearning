f8c13c3a182f359fbb69286d5cec117c6967d858
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-23 12:34:03 -0300

    v14
    Kitchen object parent

--- KitchenCaosLearning/Assets/_Assets/Scripts/KitchenObject.cs
+++ KitchenCaosLearning/Assets/_Assets/Scripts/KitchenObject.cs
@@ -4,8 +4,34 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.CleanKitchenObject();
+        }
+        this.clearCounter = clearCounter;
+        
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.Log("Error");
+        }
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
