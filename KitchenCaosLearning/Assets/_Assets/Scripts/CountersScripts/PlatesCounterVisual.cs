7b1418b3e451e42c76bb79e6292a3e6cdfa37ee1
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-03-09 18:55:58 -0300

    Plates Counter
    v30

---
+++ KitchenCaosLearning/Assets/_Assets/Scripts/CountersScripts/PlatesCounterVisual.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform platePrefab;
+    [SerializeField] private PlatesCounter platesCounter;
+
+    private List<GameObject> plateVisualGameObjectList;
+
+    private void Awake()
+    {
+        plateVisualGameObjectList = new List<GameObject>();
+    }
+    private void Start()
+    {
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateRemoved += PlatesCounter_OnPlateRemoved;
+    }
+
+    
+
+    private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+       Transform plateVisualTransform = Instantiate(platePrefab, counterTopPoint);
+        float plateOffsetY = .1f;
+        plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisualGameObjectList.Count, 0);
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
+    {
+        GameObject plateGameObject = plateVisualGameObjectList[plateVisualGameObjectList.Count - 1];
+        plateVisualGameObjectList.Remove(plateGameObject);
+        Destroy(plateGameObject);
+    }
+}
