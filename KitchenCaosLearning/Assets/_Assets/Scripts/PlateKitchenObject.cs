2c096c78cde30c94b559264ea7c442ff0205a422
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-03-09 21:27:47 -0300

    Plate complete visual
    v32

--- KitchenCaosLearning/Assets/_Assets/Scripts/PlateKitchenObject.cs
+++ KitchenCaosLearning/Assets/_Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
     private List<KitchenObjectSO> kitchenObjectSOList = new List<KitchenObjectSO>();
@@ -31,6 +38,7 @@ public class PlateKitchenObject : KitchenObject
         {
             // ingredient is not on the plate
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { kitchenObjectSO = kitchenObjectSO });
             return true;
         }
     }
